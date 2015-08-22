625e285f165ffdf5e33d114523924cfb389368a7
betty <makewithbetty@gmail.com>
2015-08-22 02:03:44 -0400

    Creating json data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,24 @@
 
 (function($) {
   $(document).ready(function() {
-    // Add your jQuery code here
+    
+    // $('.list-group-item').on('click', function(e){
+    //   $('video').remove();
+    //   $('.video-wrapper').append(
+    //     '<video controls class="panel video-player">' +
+    //       '<source src="http://treehouse-code-samples.s3.amazonaws.com/html-video-and-audio/bridge.mp4" type="video/mp4"/>' +     
+    //       '<track src="captions/vid1-captions.vtt" srclang="en" kind="subtitles" label="Captions" />' +
+    //     '</video>'
+    //   );
+
+    //   $('.list-group').find('.active').removeClass('active');
+
+    //   $(this).addClass('active');
+  
+
+    
+
+     
+    });
   });
 })(jQuery);
